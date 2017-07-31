5bed392f5a023929641de7ed7e9d2a63c6821bdb
mazlanmohdnor <lan.psis@gmail.com>
2017-08-01 01:01:40 +0800

    improve ux v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,6 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      statusBar.overlaysWebView(true);
       statusBar.backgroundColorByHexString("#FE686A");
       splashScreen.hide();
 
